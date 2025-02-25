f618db95ce3b700e381c36f9a4e8e0128a3f83dc
Flyn San <flynsarmy@gmail.com>
2025-02-26 09:03:30 +1000

    Another attempt at testing

--- demo/addons/utility_ai/tests/state_tree/root.gd
+++ demo/addons/utility_ai/tests/state_tree/root.gd
@@ -13,28 +13,55 @@ func test_no_active_children_on_unticked_root() -> void:
 ## _get_active_states() should be 1 after a tick
 func test_active_child_on_ticked_root() -> void:
 	var _root: UtilityAISTRoot = UtilityAISTRoot.new()
-	_root.add_child(UtilityAISTNode.new())
+	var _child: UtilityAISTNode = UtilityAISTNode.new()
+	_root.add_child(_child)
 	_root.tick({}, 0.1);
 	assert(_root.get_active_states().size() == 1)
+
 	_root.free()
 
+## _get_active_states() includes grandchildren
+func test_multiple_active_children_on_ticked_root() -> void:
+	var _root: UtilityAISTRoot = UtilityAISTRoot.new()
+	var _child1: UtilityAISTNode = UtilityAISTNode.new()
+	var _child1_1: UtilityAISTNode = UtilityAISTNode.new()
+	_child1.add_child(_child1_1)
+	_root.add_child(_child1)
+	_root.tick({}, 0.1);
+	assert(_root.get_active_states().size() == 2)
+	_root.free()
+
+## _get_active_states() only changes then transition_to() is called.
+func test_active_children_count_changes_on_transition() -> void:
+	var _root: UtilityAISTRoot = UtilityAISTRoot.new()
+	var _child: UtilityAISTNode = UtilityAISTNode.new()
+	_root.add_child(_child)
+	_root.tick({}, 0.1);
+	# Adding a grandchild won't affect active state count even when ticked
+	var _grandchild: UtilityAISTNode = UtilityAISTNode.new()
+	_child.add_child(_grandchild)
+	_root.tick({}, 0.1);
+	assert(_root.get_active_states().size() == 1)
+	# Transitionining triggers a recount
+	_root.transition_to('./', {}, 0.1)
+	assert(_root.get_active_states().size() == 2)
+	_root.free()
+
+
 ## _get_child_sensors() should match number of active children
-func _test_child_sensor_count() -> void:
+func test_child_sensor_count() -> void:
 	var _root: UtilityAISTRoot = UtilityAISTRoot.new()
 	var _child1: UtilityAIAngleVector2Sensor = UtilityAIAngleVector2Sensor.new()
 	var _child2: UtilityAIAngleVector2Sensor = UtilityAIAngleVector2Sensor.new()
 	var _child3: UtilityAIAngleVector2Sensor = UtilityAIAngleVector2Sensor.new()
-
+#
 	assert(_root.get_active_states().size() == 0)
 	_root.add_child(_child1)
 	assert(_root.get_child_sensors().size() == 1)
 	_root.add_child(_child2)
 	assert(_root.get_child_sensors().size() == 2)
-	# child count shouldn't change as the sensor isn't active
+	## inactive children still get counted
 	_child3.is_active = false
 	_root.add_child(_child3)
-	assert(_root.get_child_sensors().size() == 2)
-	# changes children active state aren't detected.
-	_child2.is_active = false
-	assert(_root.get_child_sensors().size() == 2)
+	assert(_root.get_child_sensors().size() == 3)
 	_root.free()
